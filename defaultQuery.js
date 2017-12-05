8206779bb3fe98a90fe79624475c712d50c2785b
erlendnils1 <erlendnils1@gmail.com>
2017-12-05 14:12:14 +0100

    Added default query for JourneyPlaner 2.0 API (transmodel graphQL)

--- defaultQuery.js
+++ defaultQuery.js
@@ -65,6 +65,73 @@ const defaultQuery = {
     }
   }
 }`,
+    'JourneyPlanner': `
+# Welcome to GraphiQL
+##################
+# GraphiQL is an in-browser IDE for writing, validating, and
+# testing GraphQL queries.
+#
+# Type queries into this side of the screen, and you will
+# see intelligent typeaheads aware of the current GraphQL type schema and
+# live syntax and validation errors highlighted within the text.
+#
+# To bring up the auto-complete at any point, just press Ctrl-Space.
+#
+# Press the run button above, or Cmd-Enter to execute the query, and the result
+# will appear in the pane to the right.
+#
+#
+#
+################## Example query for planning a journey
+#### Arguments
+{
+  trip(
+
+    #Dyrløkke - coordinates
+    fromPlace:"59.67388,10.6557"
+    #Dyrløkke - StopPlace reference (preferred solution if you know stopPlace)
+    #fromPlace:"NSR:StopPlace:5664"
+
+    #Bjørvika - coordinates
+    toPlace: "59.90804,10.756284"
+    #Bjørvika - StopPlace reference (preferred solution if you know stopPlace)
+    #toPlace: "NSR:StopPlace:70032"
+
+    numTripPatterns: 3
+    date: "2017-12-16"
+    time: "12:51:14"
+    walkReluctance: 2
+    walkBoardCost: 600
+    minTransferTime: 180
+    walkSpeed: 1.2
+    maxWalkDistance: 5000
+    wheelchair: false
+    arriveBy: false
+  )
+
+#### Requested fields
+  {
+    tripPatterns {
+      startTime
+      duration
+      walkDistance
+
+          links {
+          
+            mode
+            distance
+            line {
+              id
+              publicCode
+            }
+            pointsOnLink {
+              points
+              length
+            }
+          }
+    }
+  }
+}`,
   'Stoppestedsregisteret': `
 # Welcome to GraphiQL
 ##################
