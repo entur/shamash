8c9e2de4dd8f6f69eb9790d07e1fa304e8b5ff80
Assad Riaz <assad.riaz@entur.org>
2019-05-10 10:20:22 +0200

    Added correlationId in graphQLFetcher request header.

--- utils/graphQLFetcher.js
+++ utils/graphQLFetcher.js
@@ -1,25 +1,27 @@
 import fetch from 'isomorphic-fetch';
+import uuid from 'uuid/v4';
 
 const graphQLFetcher = graphQLParams => (
-  fetch(window.config.graphQLUrl, {
-    method: 'post',
-    headers: {
-      accept: '*/*',
-      'Content-Type': 'application/json',
-      'ET-Client-Name': 'entur-shamash',
-    },
-    body: JSON.stringify(graphQLParams)
-  })
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(responseBody) {
-    try {
-      return JSON.parse(responseBody);
-    } catch (error) {
-      return responseBody;
-    }
-  })
+    fetch(window.config.graphQLUrl, {
+        method: 'post',
+        headers: {
+            accept: '*/*',
+            'Content-Type': 'application/json',
+            'ET-Client-Name': 'entur-shamash',
+            'X-Correlation-Id': uuid(),
+        },
+        body: JSON.stringify(graphQLParams)
+    })
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (responseBody) {
+            try {
+                return JSON.parse(responseBody);
+            } catch (error) {
+                return responseBody;
+            }
+        })
 );
 
 export default graphQLFetcher;
