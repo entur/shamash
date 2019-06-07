79181b0eac5b289c09e131facc6854d842fb4143
Mats Byrkjeland <matsbyr@gmail.com>
2019-06-07 17:33:34 +0200

    Add stopPlace query example and build menu dynamically

--- components/App.js
+++ components/App.js
@@ -8,8 +8,8 @@ import '../css/custom.css';
 import { getQueryParameters } from '../utils/';
 import graphQLFetcher from '../utils/graphQLFetcher';
 
-import { tripQuery, bikeRentalStationsByBboxQuery } from '../queries/journeyplanner';
-import { stopPlaceQuery, topographicPlaceQuery } from '../queries/nsr';
+import * as journeyplannerQueries from '../queries/journeyplanner';
+import * as nsrQueries from '../queries/nsr';
 
 let logo
 
@@ -76,9 +76,9 @@ class App extends React.Component {
   getDefaultQuery() {
     if (!window.config) return
     if (window.config.serviceName === 'JourneyPlanner') {
-      return tripQuery
+      return journeyplannerQueries.tripQuery
     }
-    return topographicPlaceQuery
+    return nsrQueries.topographicPlaceQuery
   }
 
   handleClickPrettifyButton() {
@@ -140,18 +140,13 @@ class App extends React.Component {
   }
 
   renderExamplesMenu = () => {
-    if (window.config.serviceName === 'JourneyPlanner') {
-      return (
-        <GraphiQL.Menu label="Examples" title="Examples">
-          <GraphiQL.MenuItem label="trip" title="trip" onSelect={() => this.onEditQuery(tripQuery)} />
-          <GraphiQL.MenuItem label="bikeRentalStationsByBbox" title="bikeRentalStationsByBbox" onSelect={() => this.onEditQuery(bikeRentalStationsByBboxQuery)} />
-        </GraphiQL.Menu>
-      )
-    }
+    const menuEntries = Object.entries(window.config.serviceName === 'JourneyPlanner' ? journeyplannerQueries : nsrQueries)
+
     return (
       <GraphiQL.Menu label="Examples" title="Examples">
-        <GraphiQL.MenuItem label="topographicPlace" title="topographicPlace" onSelect={() => this.onEditQuery(topographicPlaceQuery)}  />
-        <GraphiQL.MenuItem label="stopPlace" title="stopPlace" onSelect={() => this.onEditQuery(stopPlaceQuery)}  />
+        { menuEntries.map(([ key, value ]) => (
+          <GraphiQL.MenuItem key={key} label={key} title={key} onSelect={() => this.onEditQuery(value)} />
+        ))}
       </GraphiQL.Menu>
     )
   }
