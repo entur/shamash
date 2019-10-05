e415909a7b3abc8492bf1b0eb1fbb0e2be53f8a5
Alicia Jasmine Miedl <alicia@addile.dev>
2019-10-05 11:16:41 +0200

    Add support for `prefers-color-scheme`
    We check for the preferred Theme to set in multiple steps:
    
    1. If theme was explicitly set (in `localStorage` already) use the specified theme.
    2. If `prefers-color-scheme: dark` is set, use dark theme.
    3. Fall back to light theme.
    
    Closes #26

---
+++ utils/getPreferredTheme.js
@@ -0,0 +1,12 @@
+const getPreferredTheme = () => {
+    const savedTheme = window.localStorage && window.localStorage.getItem('theme')
+    if (['light', 'dark'].includes(savedTheme)) {
+        return savedTheme
+    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        return 'dark'
+    } else {
+        return 'light'
+    }
+};
+
+export default getPreferredTheme;
