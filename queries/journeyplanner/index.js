79d7aebe57fc1f7e60d3e7bb911344c0ee204c13
Tom Erik Støwer <testower@gmail.com>
2020-01-03 14:58:13 +0100

    Remove v1

--- queries/journeyplanner/index.js
+++
@@ -1,3 +0,0 @@
-export { default as bikeRentalStationsByBboxQuery } from './bikeRentalStationsByBbox'
-export { default as stopPlaceQuery } from './stopPlace'
-export { default as tripQuery } from './trip'
