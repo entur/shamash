82a3813f7ba8741da7b842b161a4ec4695b2b847
Mikael Brevik <mikaelbre@gmail.com>
2020-07-28 09:20:15 +0200

    fix: Fixes scrolling and darkmode for explorer drawer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,10 +16,11 @@ import history from 'utils/history';
 import * as journeyplannerQueries from 'queries/journey-planner';
 import * as nsrQueries from 'queries/stop-places';
 import GeocoderModal from 'components/GeocoderModal';
-import './app.css';
 import './custom.css';
-import 'graphiql/graphiql.css';
 import findServiceName from 'utils/findServiceName';
+
+import explorerDarkColors from './DarkmodeExplorerColors';
+import 'graphiql/graphiql.css';
 
 let logo;
 if (getPreferredTheme() === 'dark') {
@@ -175,7 +176,7 @@ export const App = ({ services, pathname, parameters, setParameters }) => {
   } = parameters;
 
   return (
-    <div className="App">
+    <div className="App graphiql-container">
       <GraphiQLExplorer
         schema={schema}
         query={query}
@@ -185,6 +186,7 @@ export const App = ({ services, pathname, parameters, setParameters }) => {
         }
         explorerIsOpen={showExplorer}
         onToggleExplorer={toggleExplorer}
+        colors={getPreferredTheme() === 'dark' && explorerDarkColors}
       />
       <div style={{ flex: 1 }}>
         <Helmet>
---
+++ src/components/App/DarkmodeExplorerColors.js
@@ -0,0 +1,20 @@
+export default {
+  keyword: '#7cf3ff',
+  // OperationName, FragmentName
+  def: '#cc3932',
+  // FieldName
+  property: '#a5acb8',
+  // FieldAlias
+  qualifier: '#1C92A9',
+  // ArgumentName and ObjectFieldName
+  attribute: '#b58860',
+  number: '#2882F9',
+  string: '##97be7b',
+  // Boolean
+  builtin: '#D47509',
+  // Enum
+  string2: '#0B7FC7',
+  variable: '#a87f5b',
+  // Type
+  atom: '#CA9800'
+};
