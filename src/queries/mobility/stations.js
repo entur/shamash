6051c8c8b8521055f53ffd87dcf7632b1fb76e45
Tom Erik Støwer <testower@gmail.com>
2021-03-26 11:41:56 +0100

    WTK-386 Add mobility graphql api in all envs

---
+++ src/queries/mobility/stations.js
@@ -0,0 +1,32 @@
+export default {
+  query: `
+  # Welcome to GraphiQL
+  ##################
+  # GraphiQL is an in-browser IDE for writing, validating, and
+  # testing GraphQL queries.
+  #
+  # Type queries into this side of the screen, and you will
+  # see intelligent typeaheads aware of the current GraphQL type schema and
+  # live syntax and validation errors highlighted within the text.
+  #
+  # To bring up the auto-complete at any point, just press Ctrl-Space.
+  #
+  # Press the run button above, or Cmd-Enter to execute the query, and the result
+  # will appear in the pane to the right.
+  #
+  #
+  ################### Example query - fetching the 10 nearest vehicle sharing stations within 500 meters
+  {
+    stations(lat:59.913491, lon:10.757933, range: 500, count: 10) {
+      lat
+      lon
+      name
+      address
+      capacity
+      numBikesAvailable
+      numDocksAvailable
+      system { name }
+    }
+  }
+  `
+};
---
+++ src/queries/mobility/vehicles.js
@@ -0,0 +1,28 @@
+export default {
+  query: `
+  # Welcome to GraphiQL
+  ##################
+  # GraphiQL is an in-browser IDE for writing, validating, and
+  # testing GraphQL queries.
+  #
+  # Type queries into this side of the screen, and you will
+  # see intelligent typeaheads aware of the current GraphQL type schema and
+  # live syntax and validation errors highlighted within the text.
+  #
+  # To bring up the auto-complete at any point, just press Ctrl-Space.
+  #
+  # Press the run button above, or Cmd-Enter to execute the query, and the result
+  # will appear in the pane to the right.
+  #
+  #
+  ################### Example query - fetching the 25 nearest scooters within 500 meters
+  {
+    vehicles(lat:59.911491, lon:10.757933, range: 500, count: 25, formFactors: SCOOTER) {
+      lat
+      lon
+      pricingPlan { description }
+      system { name }
+    }
+  }
+  `
+};
