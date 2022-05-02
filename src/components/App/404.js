a7851d0acf94faff920f05d7700e8cc5b1adf773
Tom Erik Støwer <testower@gmail.com>
2022-05-02 09:45:08 +0200

    Add 404 page for when you are lost

---
+++ src/components/App/404.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const NotFound = () => (
+  <main>
+    <center>
+      <h1>404 - Page not found!</h1>
+      <p>
+        Oops! Itseems like you're lost, let me help you find your way back home!
+      </p>
+      <h3>
+        <a href="/">Click here to go back home</a>
+      </h3>
+    </center>
+  </main>
+);
