7f2dd445724130c4d148568b2ba7202906c32b79
Henrik Brautaset Aronsen <henrik.brautaset.aronsen@entur.org>
2025-07-01 10:56:41 +0200

    replace camelcase package with custom camelCase implementation

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -2,29 +2,42 @@
 
 const path = require('path');
 
-// Cache for the camelcase module
-let camelcase;
+// Simple camelCase implementation to replace the camelcase package
+function camelCase(str, options = {}) {
+  if (!str) return '';
 
-// Async function to load camelcase ES module
-async function getCamelcase() {
-  if (!camelcase) {
-    camelcase = (await import('camelcase')).default;
-  }
-  return camelcase;
+  // Remove non-alphanumeric characters and split into words
+  const words = str
+    .replace(/[^a-zA-Z0-9]/g, ' ')
+    .split(/\s+/)
+    .filter(word => word.length > 0);
+
+  if (words.length === 0) return '';
+
+  // Convert to camelCase
+  const firstWord = words[0].toLowerCase();
+  const restWords = words.slice(1).map(word =>
+    word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+  );
+
+  const result = firstWord + restWords.join('');
+
+  // Return PascalCase if requested
+  return options.pascalCase ?
+    result.charAt(0).toUpperCase() + result.slice(1) : result;
 }
 
 // This is a custom Jest transformer turning file imports into filenames.
 // http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
-  async processAsync(src, filename) {
+  process(src, filename) {
     const assetFilename = JSON.stringify(path.basename(filename));
 
     if (filename.match(/\.svg$/)) {
       // Based on how SVGR generates a component name:
       // https://github.com/smooth-code/svgr/blob/01b194cf967347d43d4cbe6b434404731b87cf27/packages/core/src/state.js#L6
-      const camelcaseFn = await getCamelcase();
-      const pascalCaseFilename = camelcaseFn(path.parse(filename).name, {
+      const pascalCaseFilename = camelCase(path.parse(filename).name, {
         pascalCase: true,
       });
       const componentName = `Svg${pascalCaseFilename}`;
